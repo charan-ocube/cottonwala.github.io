65fcecc3ca4be3fa2b0f606b279d5fa5cdde0ce3
Charan Biswal <60702200+charan-ocube@users.noreply.github.com>
2022-07-10 10:22:46 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php
+<!-- <?php
 
  $asin = $_GET['sku'];
 
@@ -14,11 +14,35 @@ header("location: {$url}");
 
 ?>
 
-<!--<form action="index.php" method="get" target="_blank">-->
+<form action="index.php" method="get" target="_blank">
 
-<!--<input type="text" name="sku" />-->
+<input type="text" name="sku" />
 
-<!--<input type="text" name="key"  />-->
+<input type="text" name="key"  />
 
-<!--<input type="submit" name="submit" />-->
-<!--</form> -->
+<input type="submit" name="submit" />
+</form> -->
+
+
+<form action="index.php" method="get" target="_blank">
+
+sku:<input type="text" name="sku" /> <br>
+
+key:<input type="text" name="key"  />
+
+<input type="submit" name="submit" />
+</form> 
+
+
+<?php
+if ( isset( $_GET['submit'] ) ) {
+ $asin = $_GET['sku'];
+
+$keyword = $_GET['key'];
+
+
+$qid = time();
+
+echo "https://cottonwala.com/url/?sku=$asin&key=$keyword";
+}
+?>
